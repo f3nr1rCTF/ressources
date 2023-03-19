0a139666dc0ad77e36717d7f56055785ca9a22ce
f3nr1rCTF <w0lf15@protonmail.com>
2023-03-19 23:31:14 +0100

    update install

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-sudo apt install ccrypt exa grc tmux neovim
+sudo apt install -y ccrypt exa grc tmux neovim
+
+
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 git config --global user.email "w0lf15@protonmail.com"
 git config --global user.name "f3nr1rCTF"             
@@ -21,11 +27,15 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 cd .config
 mkdir nvim
 cd nvim
-cp /Documents/ressources/
+cp /home/f3nr1r/ressources/config-mac-master/init.vim .
 
 cd / 
+cp /home/f3nr1r/ressources/config-mac-master/.tmux.conf .
 
 sudo apt install nodejs
 sudo apt install npm
 sudo npm install --global yarn
 sudo npm install --global npm
+
+#cd /home/f3nr1r/.local/share/nvim/plugged/coc.nvim
+#yarn
