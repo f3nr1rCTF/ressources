90a8326084093d9caf40c2655dd13238f6d83d98
f3nr1r <89461661+f3nr1rCTF@users.noreply.github.com>
2023-04-14 12:10:34 +0200

    Create install-tools.sh

---
+++ install-tools.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+
+
+sudo apt install -y ccrypt
+sudo apt install -y exa
+sudo apt install -y grc
+sudo apt install -y tmux
+sudo apt install -y neovim
+sudo apt install -y kitty
+sudo apt install nodejs
+sudo apt install npm
+sudo npm install --global yarn
+sudo npm install --global npm
